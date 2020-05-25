d3042269cabd7f2488282945830a33759d03fb08
f617359 <fracocafe>
2020-05-25 16:15:59 -0300

    alterando forma de buscar dados do passado.

--- feeling4bitcoin/core/twitter/historic.py
+++ feeling4bitcoin/core/twitter/historic.py
@@ -1,10 +1,14 @@
+import time
+import re
+
+from datetime import datetime
+from datetime import timedelta
+
 import GetOldTweets3 as got;
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 from core.twitter.credential import Credential
 from data.tweet_model import TweetModel
-from datetime import datetime
-from datetime import timedelta
-import time
 
 
 class Historic:
@@ -26,23 +30,47 @@ class Historic:
         print("buscando tweets do dia " + inicio.strftime("%Y-%m-%d") + " até " + data.strftime("%Y-%m-%d"))
 
         try:
+            time.sleep(30)
+
             criteria = got.manager.TweetCriteria() \
                 .setLang('en') \
                 .setQuerySearch('bitcoin') \
-                .setMaxTweets(1000) \
+                .setMaxTweets(2000) \
                 .setSince(inicio.strftime("%Y-%m-%d")).setUntil(data.strftime("%Y-%m-%d"))
 
             tweets = got.manager.TweetManager.getTweets(criteria)
 
             for tweet in tweets:
-                json = {'id': tweet.id, 'text': tweet.text, 'created_at': tweet.date}
-                print(json)
-                model.insert(json)
+                analyser = SentimentIntensityAnalyzer()
+
+                text_clean = clean(tweet.text)
+                score = analyser.polarity_scores(text_clean)
+
+                json_data = {'id': tweet.id,
+                             'text': text_clean,
+                             'created_at': tweet.date,
+                             'retweets': tweet.retweets,
+                             'neg': score['neg'],
+                             'neu': score['neu'],
+                             'pos': score['pos'],
+                             'compound': score['compound']}
+                print(json_data)
+                model.insert(json_data)
+
+            self.start(data, fim)
         except:
             print("Falha na busca de tweets")
-
-        time.sleep(30)
-        self.start(data, fim)
+            self.start(inicio, fim)
 
 
-Historic().start(datetime(2019, 8, 19), datetime(2020, 1, 1))
+def clean(text):
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r'@[\w]*', "", text)
+    text = re.sub(r'RT @[\w]*', "", text)
+    text = re.sub(r'[^a-zA-Z#!:)(=)]', ' ', text)
+    text = re.sub(' +', ' ', text)
+
+    return text
+
+
+Historic().start(datetime(2020, 4, 12), datetime(2020, 5, 1))
